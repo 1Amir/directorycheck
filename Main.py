0410e9b36147f483b356fbd68102b85efef42368
amir computing software <amirandshamar@gmail.com>
2017-05-03 13:32:31 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 import os
 
-os.walk("/home/amir")
-[x[0] for x in os.walk("/home/amir")]
-while x > 0:
-    next(os.walk('.'))[1]
-    print x
+os.walk("/home")
+[x[0] for x in os.walk("/home")]
+next(os.walk('.'))[1]
+print x
